6012d435737b0b1821c006496f93aa707d217670
Harish Chauhan <24f3003067@ds.study.iitm.ac.in>
2026-02-11 15:23:38 +0530

    Initial commit: Flask placement portal

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask,render_template,redirect,request
+from flask_login import LoginManager,login_user
+from werkzeug.security import generate_password_hash
+from models import db, User
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'placement-secret-key'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///placement.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db.init_app(app)
+
+login_manager = LoginManager()
+login_manager.login_view = 'login'
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+#------------Authentication and home routes-------------
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/login',methods=['GET','POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    else:
+        email=request.form.get('email')
+        password=request.form.get('password')
+        if email and password:
+            user = User.query.filter_by(email=email).first()
+            if user and user.check_password(password):
+                login_user(user)
+                # return redirect('/dashboard')
+        return render_template('login.html',error='Invalid email or password')
+
+# --------- DATABASE CREATION + ADMIN ----------
+with app.app_context():
+    db.create_all()
+
+    admin = User.query.filter_by(role='admin').first()
+    if not admin:
+        admin_user = User(
+            email='admin@placement.com',
+            password=generate_password_hash('admin123'),
+            role='admin'
+        )
+        db.session.add(admin_user)
+        db.session.commit()
+        print(" Admin user created")
+    else:
+        print(" Admin already exists")
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,63 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from datetime import datetime
+
+db = SQLAlchemy()
+
+
+
+class User(db.Model,UserMixin):
+    id=db.Column(db.Integer,primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False)
+    role = db.Column(db.String(20), nullable=False)   # admin / company / student
+    is_active = db.Column(db.Boolean, default=True)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+    student = db.relationship('Student', backref='user', uselist=False)
+    company = db.relationship('Company', backref='user', uselist=False)
+
+# ---------------- STUDENT
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+    name = db.Column(db.String(100))
+    roll_no = db.Column(db.String(50), unique=True)
+    department = db.Column(db.String(100))
+    cgpa = db.Column(db.Float)
+    phone = db.Column(db.String(20))
+    resume = db.Column(db.String(200))
+    is_blacklisted = db.Column(db.Boolean, default=False)
+
+# ---------------- COMPANY ----------------
+class Company(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'),nullable=False)
+    company_name = db.Column(db.String(150))
+    hr_contact = db.Column(db.String(100))
+    website = db.Column(db.String(150))
+    approval_status = db.Column(db.String(20), default='Pending')
+    is_blacklisted = db.Column(db.Boolean, default=False)
+
+# ---------------- PLACEMENT DRIVE ----------------
+class PlacementDrive(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    company_id = db.Column(db.Integer, db.ForeignKey('company.id'),nullable=False)
+    job_title = db.Column(db.String(150))
+    job_description = db.Column(db.Text)
+    eligibility = db.Column(db.String(200))
+    deadline = db.Column(db.Date)
+    status = db.Column(db.String(20), default='Pending')
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+# ---------------- APPLICATION ----------------
+class Application(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'),nullable=False)
+    drive_id = db.Column(db.Integer, db.ForeignKey('placement_drive.id'),nullable=False)
+    applied_on = db.Column(db.DateTime, default=datetime.utcnow)
+    status = db.Column(db.String(20), default='Applied')
+
+
+    __table_args__ = (
+        db.UniqueConstraint('student_id', 'drive_id', name='unique_application'),
+    )
